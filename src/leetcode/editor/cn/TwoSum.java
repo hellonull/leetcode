e20f10411b7df24b51c8e1330594613eb1f634fa
cjw <2716361955@qq.com>
2021-04-14 21:01:20 +0800

    两数之和

---
+++ src/leetcode/editor/cn/TwoSum.java
@@ -0,0 +1,67 @@
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。 
+//
+// 你可以按任意顺序返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 103 
+// -109 <= nums[i] <= 109 
+// -109 <= target <= 109 
+// 只会存在一个有效答案 
+// 
+// Related Topics 数组 哈希表 
+// 👍 10808 👎 0
+
+
+package leetcode.editor.cn;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new TwoSum().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+            for (int x = 0; x < nums.length; x++) {
+                if (map.containsKey(target - nums[x])) {
+                    return new int[]{x, map.get(target - nums[x])};
+                }
+                map.put(nums[x], x);
+            }
+            return new int[]{0, 0};
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
